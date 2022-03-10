de029dd37817b07c6bc3c1c00eb4f56a4612f59f
Takashi Tamasu <ttamasu@ucsc.edu>
2022-03-10 21:59:31 +0000

    An Application Container

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
 FROM docker.io/ubuntu:20.04
 
-RUN  apt update -y && DEBIAN_FRONTEND=noninateractive apt install -y python3-pip tzdata
-RUN python3 -m pip install django
+RUN  apt update -y && DEBIAN_FRONTEND=noninateractive apt install -y python3-pip tzdata postgresql-client
+RUN python3 -m pip install django psycopg2-binary
 
 COPY app/mysite/ /app
+
+ENV DJANGO_ADMIN_EMAIL=admn@example.com
+ENV DJANGO_ADMIN_USERNAME=admn
+ENV DJANGO_SUPERUSER_PASSWORD=admn
 
 WORKDIR /app
 
--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -70,16 +70,39 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
+import os
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'postgres': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'mysite'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'django'),
+        'HOST': os.environ.get('DB_HOST', 'postgres'),
+    },
 }
+DATABASES['default'] = DATABASES[os.environ.get('DB_ENGINE', 'sqlite')]
+#}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'mysite',
+#        'USER': 'postgres',
+#        'PASSWORD': 'django',
+#        'HOST': 'postgres',
+#    },
+#}
 
 
 # Password validation
