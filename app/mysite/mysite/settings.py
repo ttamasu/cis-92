93aa7f2809af44a6fcb3e31a493b0a87956cd771
Takashi Tamasu <ttamasu@ucsc.edu>
2022-05-03 22:12:46 +0000

    changed allowed host

--- app/mysite/mysite/settings.py
+++ app/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#yc7tpkfrbk1ie8o$p1z0mhxmf#(*s9x2d(4qdavz$a(q4tmc5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
